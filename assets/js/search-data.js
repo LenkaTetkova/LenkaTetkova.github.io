0b0cc90ee661d8f6bcff8ba2c84bf966a87c5ce4
LenkaTetkova <33063312+LenkaTetkova@users.noreply.github.com>
2025-07-04 13:36:52 +0000

    Deploying to gh-pages from @ LenkaTetkova/LenkaTetkova.github.io@fed3737f69bb0cc3aab58e1ad7bdd1bed6260635 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
+        },{id: "nav-",
+          title: "",
           description: "",
           section: "Navigation",
           handler: () => {
